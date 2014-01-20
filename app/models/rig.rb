c2efa97000a12a5d29dd95517e94c728f1cab267
Jesse <jesse.heitler@torqueim.com>
2014-01-20 19:20:01 +0000

    Removed duplicate code.

--- app/models/rig.rb
+++ app/models/rig.rb
@@ -14,33 +14,6 @@ class Rig
   def efficiency
     (watts_to_mine + watts_to_cool) / capacity
   end
-
-
-  # Methods to be a value object
   
-  def inspect
-    "#{self.class} rig_hash_rate=#{rig_hash_rate}, watts_to_mine='#{watts_to_mine}', watts_to_cool='#{watts_to_cool}'"
-  end
-  
-  def to_s
-    "Rig: #{rig_hash_rate}, '#{watts_to_mine}' + #{watts_to_cool}'"
-  end
-
-  def ==(other)
-    specs == other.specs
-  end
-
-  def eql?(other)
-    self == other
-  end
-
-  def hash
-    specs.hash
-  end
-
-  protected
-  def specs
-    [rig_hash_rate, watts_to_mine, watts_to_cool].freeze
-  end
 end
 
