689086b06163f3cd540463959710bf3703ccdec5
Theo Cushion <theo@jivatechnology.com>
2014-01-21 23:00:19 +0000

    Chore: Moved cache methods into separate module

--- lib/passive_domain.rb
+++ lib/passive_domain.rb
@@ -1,7 +1,9 @@
 require_relative "passive_domain/validation_error"
 require_relative "passive_domain/class_methods"
+require_relative "passive_domain/cache"
 
 module PassiveDomain
   include ClassMethods
+  include Cache
 end
 
