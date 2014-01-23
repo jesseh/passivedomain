bfc4da1d7c05058cebd0ae2ff4831a3f0044c98f
Theo Cushion <theo@jivatechnology.com>
2014-01-23 21:50:44 +0000

    Chore: Removed duplicate output on canned data source

--- app/models/cash_flow/data_source.rb
+++ app/models/cash_flow/data_source.rb
@@ -1,5 +1,5 @@
 module CashFlow
-  class DataSource 
+  class DataSource
 
     def initialize(mapper_class=CashFlow::Mapper)
       @mapper_class = mapper_class
@@ -14,19 +14,18 @@ module CashFlow
     def canned_data
       data = OpenStruct.new
 
-      data.rig_hash_rate               = 123
-      data.mining_difficulty           = 123
-      data.reward_amount_fractional    = 123
-      data.other_cost_fractional       = 123
-      data.facility_cost_fractional    = 123
       data.electricity_rate_fractional = 123
-      data.watts_to_mine               = 123
-      data.watts_to_cool               = 123
-      data.pool_fee_percent            = 0.07
-      data.rig_utilization             = 0.5
-      data.mining_difficulty           = 123456
       data.exchange_fee_percent        = 0.05
       data.exchange_rate               = 59
+      data.facility_cost_fractional    = 123
+      data.mining_difficulty           = 123
+      data.other_cost_fractional       = 123
+      data.pool_fee_percent            = 0.07
+      data.reward_amount_fractional    = 123
+      data.rig_hash_rate               = 123
+      data.rig_utilization             = 0.5
+      data.watts_to_cool               = 123
+      data.watts_to_mine               = 123
 
       data
     end
