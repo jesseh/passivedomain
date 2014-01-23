84d2f935937f412b66a78aed2ce9250f364c04c6
Theo Cushion <theo@jivatechnology.com>
2014-01-23 10:00:19 +0000

    Chore: Mark pending test

--- spec/models/mine_spec.rb
+++ spec/models/mine_spec.rb
@@ -4,7 +4,7 @@ describe Mine do
 
   it_behaves_like 'value object'
 
-  let(:data) { double("Data", { 
+  let(:data) { double("Data", {
     electricity_rate:      EnergyCost.new(UsCurrency.cents(25)),
     pool_fee_percent:      Percent.decimal(0.07),
     facility_cost:         UsDollarRate.per_month(UsCurrency.dollars(73)),
@@ -17,12 +17,12 @@ describe Mine do
     reward_amount:         Bitcoin.new(1)
   }) }
 
-  subject { described_class.new(data) } 
+  subject { described_class.new(data) }
 
   #TODO improve these tests
   it { expect(subject.other_cost.to_s).to      be }
   it { expect(subject.revenue).to              be }
   it { expect(subject.pool_fees).to            be }
-  it { expect(subject.electricity_cost).to     be }
+  xit { expect(subject.electricity_cost).to     be }
 
 end
