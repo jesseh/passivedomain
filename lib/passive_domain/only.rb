aaa9bfff845c40d664a9ea049f97f8c692580e02
Jesse <jesse.heitler@torqueim.com>
2014-01-27 01:41:57 +0000

    Feature: Made Only into a value object.
    This is a step towards tracking interfaces in a way that is comparable. Comparable interface will be useful for checking if a a factory returns only objects that share the same interface.

--- lib/passive_domain/only.rb
+++ lib/passive_domain/only.rb
@@ -1,47 +1,54 @@
 module PassiveDomain
   class Only
+    extend PassiveDomain
+
     def self.anything
-      new(lambda{ |raw_value| true }, nil)
+      @anything ||= new(lambda{ |raw_value| true }, nil)
     end
 
     def self.instance_of(cls)
-      new(lambda{ |raw_value| raw_value.instance_of?(cls) },
-          "instance of '#{cls.to_s}' required for %s.")
+      @instance_of ||= {}
+      @instance_of[cls] ||= new(lambda{ |raw_value| raw_value.instance_of?(cls) },
+                               "instance of '#{cls.to_s}' required for %s.")
     end
 
     def self.number
-      new(lambda{ |raw_value| raw_value.kind_of?(Numeric) },
-          "numeric type required for %s." )
+      @number ||= new(lambda{ |raw_value| raw_value.kind_of?(Numeric) },
+                      "numeric type required for %s." )
     end
 
     def self.equal_to(expected_value)
-      new(lambda{ |raw_value| raw_value == expected_value },
-          "%s must have the value '#{expected_value}'." )
+      @equal_to ||= {}
+      @equal_to[expected_value] ||= new(lambda{ |raw_value| raw_value == expected_value },
+                                        "%s must have the value '#{expected_value}'." )
     end
 
     def self.string
-      new(lambda{ |raw_value| raw_value.kind_of?(String) },
-          "string required for %s." )
+      @string ||= new(lambda{ |raw_value| raw_value.kind_of?(String) },
+                      "string required for %s." )
     end
 
     def self.positive_integer
-      new(lambda{ |raw_value| raw_value.kind_of?(Integer) && raw_value >= 0 },
-        "positive integer required for %s." )
+      @positive_integer ||= new(lambda{ |raw_value| raw_value.kind_of?(Integer) && raw_value >= 0 },
+                               "positive integer required for %s." )
     end
 
     def self.positive_number
-      new(lambda{ |raw_value| raw_value.kind_of?(Numeric) && raw_value >= 0 },
-          "positive number required for %s." )
+      @positive_number ||= new(lambda{ |raw_value| raw_value.kind_of?(Numeric) && raw_value >= 0 },
+                               "positive number required for %s." )
     end
 
     def self.number_within(range)
-      new(lambda{ |raw_value| raw_value.kind_of?(Numeric) && range.include?(raw_value) },
-          "positive number required for %s." )
+      @number_within ||= {}
+      @number_within[range] ||= new(lambda{ |raw_value| raw_value.kind_of?(Numeric) && range.include?(raw_value) },
+                                    "positive number required for %s." )
     end
+
 
     def initialize(test_lambda, fail_message)
       @test_lambda = test_lambda
-      @fail_message = fail_message
+      @fail_message = fail_message.freeze
+      freeze
     end
 
     def check(name, raw_value)
@@ -50,8 +57,28 @@ module PassiveDomain
       end
     end
 
-    private
-
     attr_reader :test_lambda, :fail_message
-  end
+    protected :test_lambda, :fail_message
+
+    def inspect
+      "#{self.class} test_lambda=#{test_lambda}, fail_message='#{fail_message}'"
+    end
+    
+    def to_s
+      inspect
+    end
+
+    def ==(other)
+      other.instance_of?(self.class) && test_lambda == other.test_lambda && fail_message == other.fail_message
+    end
+
+    def eql?(other)
+      self == other
+    end
+
+    def hash
+      [self.class, test_lambda, fail_message].hash
+    end
+
+    end
 end
---
+++ spec/lib/passive_domain/only_spec.rb
@@ -0,0 +1,90 @@
+require "spec_helper"
+
+require_dependency Rails.root.join('lib', 'passive_domain').to_s
+
+
+describe PassiveDomain::Only do
+  subject { described_class.positive_number }
+
+  it_behaves_like('value object')
+
+  describe "Class constructors" do
+    let(:v1) { described_class.send(method_symbol) }
+    let(:v2) { described_class.send(method_symbol) }
+    subject { described_class.positive_number }
+
+    context "anything" do
+      let(:method_symbol) { :anything }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+    end
+
+    context "instance_of" do
+      let(:v1) { described_class.send(method_symbol, Class) }
+      let(:v2) { described_class.send(method_symbol, Class) }
+      let(:v3) { described_class.send(method_symbol, Numeric) }
+      let(:method_symbol) { :instance_of }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+      it "does inequality correctly" do
+        expect(v1).to_not eq v3
+      end
+    end
+
+    context "number" do
+      let(:method_symbol) { :number }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+    end
+
+    context "equal_to" do
+      let(:v1) { described_class.send(method_symbol, 1) }
+      let(:v2) { described_class.send(method_symbol, 1) }
+      let(:v3) { described_class.send(method_symbol, 2) }
+      let(:method_symbol) { :equal_to }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+      it "does inequality correctly" do
+        expect(v1).to_not eq v3
+      end
+    end
+
+    context "string" do
+      let(:method_symbol) { :string }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+    end
+
+    context "positive_integer" do
+      let(:method_symbol) { :positive_integer }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+    end
+
+    context "positive_number" do
+      let(:method_symbol) { :positive_number }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+    end
+
+    context "number_within" do
+      let(:v1) { described_class.send(method_symbol, 1...4) }
+      let(:v2) { described_class.send(method_symbol, 1...4) }
+      let(:v3) { described_class.send(method_symbol, 2...4) }
+      let(:method_symbol) { :number_within }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+      it "does inequality correctly" do
+        expect(v1).to_not eq v3
+      end
+    end
+  end
+end
