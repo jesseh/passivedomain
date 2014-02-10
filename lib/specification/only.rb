994a0802466ea9176c03398a94ad503602eaf59f
Jesse <jesse.heitler@torqueim.com>
2014-02-10 08:54:24 +0000

    WIP: Copied Only into specification.

---
+++ lib/specification/only.rb
@@ -0,0 +1,154 @@
+require_dependency Rails.root.join('lib', 'passive_domain').to_s
+
+module Specification
+  class Only
+    extend PassiveDomain
+
+    class AnyClass; end
+
+    def self.anything
+      @anything ||= new(lambda{ |raw_value| true },
+                        nil,
+                        ->{ [nil, "any string", :any_symbol, AnyClass, rand(-1000..1000)].sample }
+                       )
+    end
+
+    def self.instance_of(cls)
+      @instance_of ||= {}
+      @instance_of[cls] ||= new(lambda{ |raw_value| raw_value.instance_of?(cls) },
+                               "instance of '#{cls.to_s}' required for %s.",
+                               lambda { 
+                                  responder = Interface.for_class(cls).responder
+                                  responder.nil? ? nil : cls.new(responder)
+                               }) 
+    end
+
+    def self.number
+      @number ||= new(lambda{ |raw_value| raw_value.kind_of?(Numeric) },
+                      "numeric type required for %s.",
+                      ->{ rand -1000000..1000000 }
+                     )
+    end
+
+    def self.equal_to(expected_value)
+      @equal_to ||= {}
+      @equal_to[expected_value] ||= new(lambda{ |raw_value| raw_value == expected_value },
+                                        "%s must have the value '#{expected_value}'.",
+                                       expected_value)
+    end
+
+    def self.symbol
+      @symbol ||= new(lambda{ |raw_value| raw_value.kind_of?(Symbol) },
+                      "symbol required for %s.",
+                      ->{ [:standin_symbol_1, :standin_symbol_2, :standin_symbol_3].sample })
+    end
+
+    def self.symbol_or_string
+      @symbol_or_string ||= new(lambda{ |raw_value| raw_value.kind_of?(Symbol) ||
+                                                    raw_value.kind_of?(String) },
+                                "symbol or string required for %s.",
+                                ->{ ["standin symbol or string", :standin_symbol_or_string].sample }
+                               )
+    end
+
+    def self.string_or_nil
+      @string_or_nil ||= new(lambda{ |raw_value|  raw_value.kind_of?(NilClass) ||
+                                                  raw_value.kind_of?(String) },
+                                "string or nil required for %s.",
+                                ->{ [nil, "standin string or nil"].sample }
+                            )
+    end
+
+    def self.string_symbol_or_nil
+      @string_symbol_or_nil ||= new(lambda{ |raw_value| raw_value.kind_of?(NilClass) ||
+                                                        raw_value.kind_of?(Symbol)  ||
+                                                        raw_value.kind_of?(String) },
+                                      "string, symbol, or nil required for %s.",
+                                      ->{ [nil, "standin string symbol or nil", :standin_string_symbol_or_nil].sample }
+                                   )
+    end
+
+    def self.string
+      @string ||= new(lambda{ |raw_value| raw_value.kind_of?(String) },
+                      "string required for %s.",
+                      ->{ rand(10) < 3 ? "" : "standin string #{rand(36**5).to_s(36)}" }
+                     )
+    end
+
+    def self.positive_integer
+      @positive_integer ||= new(lambda{ |raw_value| raw_value.kind_of?(Integer) && raw_value >= 0 },
+                                "positive integer required for %s.",
+                                ->{ rand(10) < 2 ? 0 : rand(0..100000) }
+                               )
+    end
+
+    def self.positive_number
+      @positive_number ||= new(lambda{ |raw_value| raw_value.kind_of?(Numeric) && raw_value >= 0 },
+                               "positive number required for %s.",
+                               ->{ rand(10) < 2 ? 0 : (rand(0..100000) * rand) }
+                              )
+    end
+
+    def self.number_within(range)
+      @number_within ||= {}
+      @number_within[range] ||= new(lambda{ |raw_value| raw_value.kind_of?(Numeric) && range.include?(raw_value) },
+                                    "positive number required for %s.",
+                                    ->{ [range.max, range.min, rand(range)].sample }
+                                   )
+    end
+
+    def self.instance_array(cls)
+      @instance_array ||= {}
+      @instance_array[cls] ||= new(lambda{ |raw_value| raw_value.all? { |x| x.instance_of?(cls) } },
+                                    "an array of #{cls} instances required for %s.",
+                                  ->{ [] })  # empty array because the expected class may not be easy to instantiate.
+    end
+
+    attr_reader :test_lambda, :fail_message
+    protected :test_lambda, :fail_message
+
+    def initialize(test_lambda, fail_message, standin_lambda=nil)
+      @test_lambda = test_lambda
+      @fail_message = fail_message.freeze
+      standin_lambda = ->{ nil } unless standin_lambda.respond_to?(:call) 
+      @standin_generator = as_generator standin_lambda
+      freeze
+    end
+
+    def standin_value
+      @standin_generator.next
+    end
+
+    def check(name, raw_value)
+      unless test_lambda.call(raw_value)
+        fail_message % name 
+      end
+    end
+
+
+    def inspect
+      "#{self.class} test_lambda=#{test_lambda}, fail_message='#{fail_message}'"
+    end
+    
+    def to_s
+      inspect
+    end
+
+    def ==(other)
+      other.instance_of?(self.class) && test_lambda == other.test_lambda && fail_message == other.fail_message
+    end
+
+    def eql?(other)
+      self == other
+    end
+
+    def hash
+      [self.class, test_lambda, fail_message].hash
+    end
+
+    private
+    def as_generator(value_lambda)
+      Enumerator.new{ |y| loop{ y << value_lambda.call } }
+    end
+  end
+end
---
+++ spec/lib/specification/only_spec.rb
@@ -0,0 +1,116 @@
+require "spec_helper"
+
+require_dependency Rails.root.join('lib', 'specification').to_s
+
+
+describe Specification::Only do
+  subject { described_class.positive_number }
+
+  it_behaves_like('value object')
+
+  describe "Class constructors" do
+    let(:v1) { described_class.send(method_symbol) }
+    let(:v2) { described_class.send(method_symbol) }
+    subject { described_class.positive_number }
+
+    context "anything" do
+      let(:method_symbol) { :anything }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+    end
+
+    context "instance_of" do
+      let(:v1) { described_class.send(method_symbol, Class) }
+      let(:v2) { described_class.send(method_symbol, Class) }
+      let(:v3) { described_class.send(method_symbol, Numeric) }
+      let(:method_symbol) { :instance_of }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+      it "does inequality correctly" do
+        expect(v1).to_not eq v3
+      end
+    end
+
+    context "number" do
+      let(:method_symbol) { :number }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+    end
+
+    context "equal_to" do
+      let(:v1) { described_class.send(method_symbol, 1) }
+      let(:v2) { described_class.send(method_symbol, 1) }
+      let(:v3) { described_class.send(method_symbol, 2) }
+      let(:method_symbol) { :equal_to }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+      it "does inequality correctly" do
+        expect(v1).to_not eq v3
+      end
+    end
+
+    context "string" do
+      let(:method_symbol) { :string }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+    end
+
+    context "positive_integer" do
+      let(:method_symbol) { :positive_integer }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+    end
+
+   context "positive_number" do
+      let(:method_symbol) { :positive_number }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+    end
+
+    context "number_within" do
+      let(:v1) { described_class.send(method_symbol, 1...4) }
+      let(:v2) { described_class.send(method_symbol, 1...4) }
+      let(:v3) { described_class.send(method_symbol, 2...4) }
+      let(:method_symbol) { :number_within }
+      it "does equality correctly" do
+        expect(v1).to eq v2
+      end
+      it "does inequality correctly" do
+        expect(v1).to_not eq v3
+      end
+    end
+
+    context "instance_array" do
+      let(:cls) { Class.new }
+      let(:v2) { described_class.send(method_symbol, cls) }
+      let(:v3) { described_class.send(method_symbol, Class.new) }
+      let(:method_symbol) { :instance_array }
+
+      subject { described_class.send(method_symbol, cls) }
+
+      it "does equality correctly" do
+        expect(subject).to eq v2
+      end
+
+      context "valid input" do
+        it "checks the class of the instances in the array" do
+          expect(subject.check("test code", [cls.new, cls.new])).to be_nil
+        end
+      end
+
+      context "invalid input" do
+        it "checks the class of the instances in the array" do
+          expect(subject.check("test code", [1,2,3])).to_not be_nil
+        end
+      end
+    end
+
+  end
+end
